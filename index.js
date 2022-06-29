a7eb2075b1779e57ea6121a5fbef6ca0bff4784e
Debonair2 <dareblessing12@gmail.com>
2022-06-29 23:47:34 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+let score = document.getElementById("home-score").textContent
+score = 0
+
+function increment1() {
+    score += 1
+    document.getElementById("home-score").textContent = score
+
+}
+
+function increment2() {
+    score += 2
+    document.getElementById("home-score").textContent = score
+
+}
+
+function increment3() {
+    score += 3
+    document.getElementById("home-score").textContent = score
+
+}
+
+
+
+
+
+
+let score2 = document.getElementById("guest-score").textContent
+score2 = 0
+
+function increment21() {
+    score2 += 1
+    document.getElementById("guest-score").textContent = score2
+
+}
+
+function increment22() {
+    score2 += 2
+    document.getElementById("guest-score").textContent = score2
+
+}
+
+function increment23() {
+    score2 += 3
+    document.getElementById("guest-score").textContent = score2
+
+}
+
+
+
